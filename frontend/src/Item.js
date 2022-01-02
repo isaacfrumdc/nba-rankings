7f35658b133207a85111b5321e6152ebc0f64284
isaacfrumkin <ifrumki1@jhu.edu>
2022-01-01 23:36:19 -0800

    icon buttons

--- frontend/src/Item.js
+++ frontend/src/Item.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { IconButton } from '@chakra-ui/react'
+import { AddIcon, ArrowDownIcon, ArrowUpIcon, CloseIcon, StarIcon } from '@chakra-ui/icons'
 
 const Item = ({ item, onAddPlayer, onRemovePlayer, onMoveUp, onMoveDown, listType }) => (
     <div style={{ display: 'flex' }}>
@@ -6,25 +8,36 @@ const Item = ({ item, onAddPlayer, onRemovePlayer, onMoveUp, onMoveDown, listTyp
         <span style={{ width: '30%' }}>{item.team_city} {item.team_name}</span>&nbsp;
         <span style={{ width: '10%' }}>{item.position}</span>&nbsp;
         {listType === 'All' && <span>
-            <button type="button" onClick={() => onAddPlayer(item)}>
+            {/* <button type="button" onClick={() => onAddPlayer(item)}>
                 Add to Ranking
-            </button>
+            </button> */}
+            <IconButton aria-label='Add to Ranking' icon={<AddIcon />} colorScheme='green'
+                type="button" onClick={() => onAddPlayer(item)} />
         </span>}
         {listType === 'Rank' && <span>
-            <button type="button" onClick={() => onMoveUp(item)}>
+            {/* <button type="button" onClick={() => onMoveUp(item)}>
                 Up
-            </button>
+            </button> */}
+            <IconButton aria-label='Move Up' icon={<ArrowUpIcon />}
+                type="button" size='sm' onClick={() => onMoveUp(item)} />
         </span>}
         {listType === 'Rank' && <span>
-            <button type="button" onClick={() => onMoveDown(item)}>
+            {/* <button type="button" onClick={() => onMoveDown(item)}>
                 Down
-            </button>
+            </button> */}
+            <IconButton aria-label='Move Down' icon={<ArrowDownIcon />}
+                type="button" size='sm' onClick={() => onMoveDown(item)} />
         </span>}
         {listType === 'Rank' && <span>
-            <button type="button" onClick={() => onRemovePlayer(item)}>
+            {/* <button type="button" onClick={() => onRemovePlayer(item)}>
                 Remove
-            </button>
+            </button> */}
+            <IconButton aria-label='Remove Player' icon={<CloseIcon />} colorScheme='red'
+                type="button" size='sm' onClick={() => onRemovePlayer(item)} />
         </span>}
+        {listType === 'Consensus' && <span>
+        <IconButton aria-label='Remove Player' icon={<StarIcon />} colorScheme='yellow'
+                type="button" isDisabled size='sm' />            </span>}
     </div>
 );
 
